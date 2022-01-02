608ba27178a0883b610121165b2f2463f494c8e6
Conchylicultor <etiennefg.pot@gmail.com>
2022-01-02 16:41:12 +0100

    Add caching and extra metadata

---
+++ imdb2sheet/README.md
@@ -0,0 +1,13 @@
+# IMDB Visualization
+
+1. Export the IMDB ratings to `rating.csv`
+
+2. Export the OMDB API key:
+
+```sh
+export OMDB_KEY=abcdef
+```
+
+3. Run the `imdb2sheet.py` to fetch the additional metadata (language, actors,...)
+
+4. Run the `visualization.ipnb`
--- imdb2sheet/imdb2sheet.py
+++ imdb2sheet/imdb2sheet.py
@@ -7,6 +7,7 @@ import dataclasses
 import datetime
 import enum
 import json
+import os
 import pathlib
 import re
 from typing import Optional
@@ -56,7 +57,7 @@ class MovieType(epy.StrEnum):
 Genre = str
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class Movie:
     id: str
     title: str
@@ -70,6 +71,19 @@ class Movie:
     imdb_rating: float
     my_rating: int
     date_rated: datetime.date
+
+    # Extra metadata
+    plot: str = None
+    writers: list[str] = None
+    actors: list[str] = None
+    languages: list[str] = None
+    countries: list[str] = None
+    pg_rating: str = None
+    poster: str = None
+    awards: str = None
+    box_office: int = None
+    rotten_rating: int = None
+    metacritic_rating: int = None
 
     @property
     def url(self) -> str:
@@ -89,9 +103,9 @@ class Movie:
             return cls(
                 id=row["Const"],
                 title=row["Title"],
-                type=MovieType(camel2snake(row["Title Type"])),
-                genres=[g for g in row["Genres"].split(", ")],
-                directors=[d for d in row["Directors"].split(", ")],
+                type=MovieType(_camel2snake(row["Title Type"])),
+                genres=_list_str(row["Genres"]),
+                directors=_list_str(row["Directors"]),
                 year=int(row["Year"]),
                 release_date=release_date,
                 runtime=runtime,
@@ -103,9 +117,127 @@ class Movie:
                 ).date(),
             )
 
+    def replace_with_metadata(self) -> Movie:
+        """Update with additional metadata.
 
-# TODO(epot):
-# {"Title":"Guardians of the Galaxy Vol. 2","Year":"2017","Rated":"PG-13","Released":"05 May 2017","Runtime":"136 min","Genre":"Action, Adventure, Comedy","Director":"James Gunn","Writer":"James Gunn, Dan Abnett, Andy Lanning","Actors":"Chris Pratt, Zoe Saldana, Dave Bautista","Plot":"The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.","Language":"English","Country":"United States","Awards":"Nominated for 1 Oscar. 15 wins & 58 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.6/10"},{"Source":"Rotten Tomatoes","Value":"85%"},{"Source":"Metacritic","Value":"67/100"}],"Metascore":"67","imdbRating":"7.6","imdbVotes":"622,462","imdbID":"tt3896198","Type":"movie","DVD":"22 Aug 2017","BoxOffice":"$389,813,101","Production":"N/A","Website":"N/A","Response":"True"}
+        Example:
+
+        ```
+        {
+            "Title":"Guardians of the Galaxy Vol. 2",
+            "Year":"2017",
+            "Rated":"PG-13",
+            "Released":"05 May 2017",
+            "Runtime":"136 min",
+            "Genre":"Action, Adventure, Comedy",
+            "Director":"James Gunn",
+            "Writer":"James Gunn, Dan Abnett, Andy Lanning",
+            "Actors":"Chris Pratt, Zoe Saldana, Dave Bautista",
+            "Plot":"The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.",
+            "Language":"English",
+            "Country":"United States",
+            "Awards":"Nominated for 1 Oscar. 15 wins & 58 nominations total","
+            Poster":"https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+            "Ratings":[
+                {"Source":"Internet Movie Database","Value":"7.6/10"},
+                {"Source":"Rotten Tomatoes","Value":"85%"},
+                {"Source":"Metacritic","Value":"67/100"},
+            ],
+            "Metascore":"67",
+            "imdbRating":"7.6",
+            "imdbVotes":"622,462",
+            "imdbID":"tt3896198",
+            "Type":"movie",
+            "DVD":"22 Aug 2017",
+            "BoxOffice":"$389,813,101",
+            "Production":"N/A",
+            "Website":"N/A",
+            "Response":"True",
+        }
+        ```
+
+        Note: Series have slightly different fields
+
+        """
+        if self.plot is not None:
+            print(f"Reusing {self.title}")
+            return self
+        print(f"Fetching {self.title}")
+
+        omdb_api = OmdbAPI()
+        out = omdb_api.query(self.id)
+
+        with epy.maybe_reraise(f"{out=}\n"):
+            ratings = out["Ratings"]
+            rotten_ratings = [
+                r["Value"] for r in ratings if r["Source"] == "Rotten Tomatoes"
+            ]
+            if rotten_ratings:
+                (rotten_rating,) = rotten_ratings
+                rotten_rating = int(rotten_rating.rstrip("%"))
+            else:
+                rotten_rating = -1
+
+            if (metascore := out["Metascore"]) != "N/A":
+                metacritic_rating = int(metascore)
+            else:
+                metacritic_rating = -1
+
+            if (box_office := out.get("BoxOffice", "N/A")) != "N/A":
+                box_office = int(box_office.lstrip("$").replace(",", ""))
+            else:
+                box_office = 0
+
+            return dataclasses.replace(
+                self,
+                plot=out["Plot"],
+                writers=_list_str(out["Writer"]),
+                actors=_list_str(out["Actors"]),
+                languages=_list_str(out["Language"]),
+                countries=_list_str(out["Country"]),
+                pg_rating=out["Rated"],
+                poster=out["Poster"],
+                awards=out["Awards"],
+                box_office=box_office,
+                rotten_rating=rotten_rating,
+                metacritic_rating=metacritic_rating,
+            )
+
+    def to_json(self) -> str:
+        value = dataclasses.asdict(self)
+        value = json.dumps(value, default=serialize_json)
+        return value
+
+    @classmethod
+    def from_json(cls, json_value: str) -> Movie:
+        init_kwargs = json.loads(json_value, object_hook=deserialize_json)
+        init_kwargs["type"] = MovieType(init_kwargs["type"])
+        return cls(**init_kwargs)
+
+
+def serialize_json(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, datetime.date):
+        name = "date"
+        val = obj.isoformat()
+    elif isinstance(obj, datetime.timedelta):
+        name = "timedelta"
+        val = obj.total_seconds()
+    else:
+        raise TypeError(f"Type {type(obj)} not serializable")
+    return f"{name}://{val}"
+
+
+def deserialize_json(json_dict):
+    for k, v in json_dict.items():
+        if isinstance(v, str):
+            if v.startswith("date://"):
+                json_dict[k] = datetime.date.fromisoformat(v[len("date://") :])
+            elif v.startswith("timedelta://"):
+                json_dict[k] = datetime.timedelta(
+                    seconds=float(v[len("timedelta://") :])
+                )
+    return json_dict
 
 
 def _add_arguments(url, **arguments):
@@ -119,7 +251,7 @@ class OmdbAPI:
     """OMDB API."""
 
     def query(self, imdb_id):
-        api_key = ""
+        api_key = os.environ["OMDB_KEY"]
         url = f"http://www.omdbapi.com/"
         url = _add_arguments(
             url,
@@ -130,39 +262,54 @@ class OmdbAPI:
         # Fetch and parse result
         resp = requests.get(url)
         out = json.loads(resp.text)
-        if out.get("Response", True) == "False":
+        if out["Response"] != "True":
             status_message = out["Error"]
             raise ValueError(f"Invalid request: {url}.\n{status_message=}")
         return out
 
 
-def load_movies(fetch_extra=False) -> list[Movie]:
+def load_movies() -> list[Movie]:
     # 1) Fetch IMDB ratings
     # TODO(epot): Ideally, should fetch the public list
     # url = "https://www.imdb.com/user/ur42567646/ratings/export"
     # resp = requests.get(url, allow_redirects=True)
     # resp.text
     p = pathlib.Path(__file__).parent / "ratings.csv"
-    with p.open(errors="replace") as f:
+    with p.open(encoding="windows-1252") as f:
         movies = [Movie.from_ratings(row) for row in csv.DictReader(f)]
 
-    if not fetch_extra:
-        return movies
+    return movies
 
-    # 3) Fetch missing metadata (thumbnail, country,...)
-    omdb_api = OmdbAPI()
-    for movie in movies:
-        out = omdb_api.query("tt0068646")
-    print(out)
+
+def load_movies_extra() -> list[Movie]:
+    out_path = pathlib.Path(__file__).parent / "cache.jsonl"
+    movies = [Movie.from_json(v) for v in out_path.read_text().splitlines() if v]
+    return movies
 
 
 def main():
     """Main script."""
+    # Load movies and pre-computed movies
     movies = load_movies()
+    movies_extra = {m.id: m for m in load_movies_extra()}
+
+    # Use pre-computed version when available
+    movies = [movies_extra.get(m.id, m) for m in movies]
+
+    # Fetch extra movies
+    extra_movies = [m.replace_with_metadata().to_json() for m in movies[:1200]]
+
+    # Save output
+    out_path = pathlib.Path(__file__).parent / "cache.jsonl"
+    out_path.write_text("\n".join(extra_movies))
 
 
-def camel2snake(name: str) -> str:
+def _camel2snake(name: str) -> str:
     return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+
+
+def _list_str(names: str) -> list[str]:
+    return [d for d in names.split(", ")]
 
 
 if __name__ == "__main__":
